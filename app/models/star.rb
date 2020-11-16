df7de713a763e4df160019e8b62bb4876be2d3d5
Joanna Chaillou <joanna.chaillou@edu.escp.eu>
2020-11-16 18:47:30 +0100

    resolved through:

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,7 +1,8 @@
 class Star < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  has_many :reviews, :through :bookings
+  has_many :reviews, through: :bookings
+
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { only_integer: true }
 end
