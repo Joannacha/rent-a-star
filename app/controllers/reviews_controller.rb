4ae9dad3e55189e97ab66c24f5d1d4e82575e9f4
Joanna Chaillou <69638927+Joannacha@users.noreply.github.com>
2020-11-17 18:30:25 +0100

    Update reviews_controller.rb
    notice added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,7 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.booking = @booking
     if @review.save
-      redirect_to booking_path(@booking)
+      redirect_to booking_path(@booking), notice: "You successfully added a review!"
     else
       render 'bookings/show'
     end
