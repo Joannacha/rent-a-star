bcd1ef124b45d4ca4370b5cf59fd790be1fdeb6a
Joanna Chaillou <joanna.chaillou@edu.escp.eu>
2020-11-17 16:08:40 +0100

    added reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :stars
+  resources :stars do
+    resources :bookings, only: [ :new, :create ]
+  end
+  resources :bookings, only: [ :show, :destroy ] do
+    resources :reviews, only: [ :create ]
+    collection do
+      get :my_bookings
+      get :my_listings
+    end
+  end
 end
