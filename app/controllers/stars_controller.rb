aa2cd6c73ec73dab68537143cb06e198e2e9e5b8
Thibault Sordet <sordetthibault@gmail.com>
2020-11-17 14:09:56 +0100

    add destroy and show action

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,2 +1,17 @@
 class StarsController < ApplicationController
+  before_action :set_star, only: [:show, :update, :destroy]
+
+  def show
+  end
+
+  def destoy
+    @star = Star.find(params[:id])
+    @star.destroy
+  end
+
+  private
+
+  def set_star
+    @star = Star.find(params[:id])
+  end
 end
