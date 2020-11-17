26414f077f28a17b7c3baa2082469da6d28cecb9
Joanna Chaillou <69638927+Joannacha@users.noreply.github.com>
2020-11-17 18:35:16 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :stars do
-    resources :bookings, only: [ :new, :create ]
+    resources :bookings, only: [ :create ]
   end
   resources :bookings, only: [ :show, :destroy ] do
     resources :reviews, only: [ :create ]
-    collection do
-      get :my_bookings
-      get :my_listings
-    end
   end
+  get '/my_bookings', to: "bookings#my_bookings"
+  get '/my_listings', to: "bookings#my_listings"
 end
