fe3c87f57558ab90db15f47fd9590100cf699452
Thibault Sordet <sordetthibault@gmail.com>
2020-11-17 11:40:35 +0100

    fix the signup issue for fn ln un

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+before_action :authenticate_user!
+before_action :configure_permitted_parameters, if: :devise_controller?
+protected
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+    user_params.permit(:email,
+                       :password,
+                       :password_confirmation,
+                       :first_name,
+                       :last_name,
+                       :username
+                       )
+  end
+  devise_parameter_sanitizer.permit(:account_update) do |user_params|
+    user_params.permit(:email,
+                       :first_name,
+                       :last_name,
+                       :username,
+                       :phone,
+                       :password,
+                       :password_confirmation,
+                       :current_password,
+                        )
+  end
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable,  and :omniauthable, :trackable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :stars
   has_many :bookings
   validates :first_name, presence: true
   validates :last_name, presence: true
+  attr
 end
