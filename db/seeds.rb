556070218f9a2a250785ea79a88ac7ad269c4de8
Thibault Sordet <sordetthibault@gmail.com>
2020-11-18 14:40:08 +0100

    seed the star

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do
+  star = Star.new(
+    name: Faker::Space.unique.star,
+    price: rand(1..20000),
+    user_id: rand(2..6)
+  )
+  star.save!
+end
