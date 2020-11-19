7d0dea0e7067f7566ea36449b93d00508979db39
Joanna Chaillou <joanna.chaillou@edu.escp.eu>
2020-11-19 17:16:39 +0100

    commit change to seed on master

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Booking.delete_all
+Star.delete_all
+
 puts "Starting the seed"
-
-
-
 
 10.times do
   users = User.new(
